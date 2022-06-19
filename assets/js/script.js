485d2bbc7e15b83aee487effa78415ec14101788
AASanusi <alaminm.sanusi@gmail.com>
2022-06-19 19:07:12 +0000

    Add let variable

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,9 @@ const answers =  Array.from(document.getElementsByClassName("answer-text"));
 const progressText =  document.getElementById("progressText");
 const scoreText =  document.getElementById("score");
 const progressTextFull =  document.getElementById("progressTextFull");
+
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestion = [];
