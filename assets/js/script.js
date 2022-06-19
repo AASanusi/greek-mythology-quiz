1bbcb6785d01d9b8a276bd1202b2c6261423bd2f
AASanusi <alaminm.sanusi@gmail.com>
2022-06-19 18:58:27 +0000

    Target id and class in game div

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+const question =  document.getElementById("question");
+const answers =  Array.from(document.getElementsByClassName("answer-text"));
+const progressText =  document.getElementById("progressText");
+const scoreText =  document.getElementById("score");
+const progressTextFull =  document.getElementById("progressTextFull");
