9ba636f5132d63e002bdea92066bc7df06e7bef7
AASanusi <alaminm.sanusi@gmail.com>
2022-06-20 11:49:42 +0000

    Add eventlisteners to the quiz and add correct and incorrect toggles

--- assets/js/script.js
+++ assets/js/script.js
@@ -101,3 +101,17 @@ getNewQuestion = () => {
     availableQuestions.splice(questionsIndex, 1);
     acceptingAnswers = true;
 };
+
+answers.forEach(choice => {
+    choice.addEventListener("click", e => {
+        if(!acceptingAnswers) return
+        acceptingAnswers = false
+        const selectedChoice = e.target
+        const selectedAnswer = selectedChoice.dataset["number"]
+        let classToApply = selectedAnswer == currentQuestion.answer ? "correct" : "incorrect"
+
+        if(classToApply === "correct") {
+            
+        }
+    })
+})
