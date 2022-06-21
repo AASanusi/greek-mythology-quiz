c018903c5cb68340490cb712c6299fe356bc3e73
AASanusi <alaminm.sanusi@gmail.com>
2022-06-21 12:40:37 +0000

    Create local storage to push scores and save to highscores

--- assets/js/end.js
+++ assets/js/end.js
@@ -1,13 +1,13 @@
 /* Parts of this project was assisted and inspired by
-* simplestepcode.com and watching youtube tutorial
-* from Brian Design (https://www.youtube.com/watch?v=f4fB9Xg2JEY)
-*/
+ * simplestepcode.com and watching youtube tutorial
+ * from Brian Design (https://www.youtube.com/watch?v=f4fB9Xg2JEY)
+ */
 
 // Variables to get elements by Ids and classess
 const uname = document.getElementById("uname");
 const totalScore = document.getElementById("totalScore");
 const saveScoreButton = document.getElementById("saveScoreButton");
-const mostRecentScore = document.querySelector("#mostRecentScore");
+const mostRecentScore = localStorage.getItem("mostRecentScore");
 
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
@@ -15,7 +15,30 @@ const maxHighScores = 5;
 
 finalScore.innerText = mostRecentScore;
 
-// To allow users to "save" once details have been filled in box
+/* To allow users to "save" once details have been filled in box,
+* To send scores to highscores and not to automatically refresh,
+* show scores on highscores table
+*/
 uname.addEventListener("keyup", () => {
-    saveScoreButton.disabled = !uname.value 
+    saveScoreButton.disabled = !uname.value
 });
+
+saveHighScore = e => {
+    e.preventDefault();
+
+    const score = {
+        score: mostRecentScore,
+        name: uname.value
+    };
+
+    highScores.push(score);
+
+    highScores.sort((a,b) => {
+        return b.score - a.score;
+    });
+
+    highScores.splice(5);
+
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    window.location.assign("highscores.html");
+}
